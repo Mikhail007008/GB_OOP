1daa26b1f77d03c43658c06e5c6cfe33e74e0646
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-21 16:17:02 +0300

    Update Peasant.java

--- src/Units/Peasant.java
+++ src/Units/Peasant.java
@@ -1,5 +1,4 @@
 package Units;
-<<<<<<< HEAD
 
 import Edits.Coord;
 
@@ -20,32 +19,4 @@ public class Peasant extends Unit {
 
     @Override
     public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
-=======
-import java.util.ArrayList;
-
-import Coord;
-
-public class Peasant extends Unit {
-    private int endurance;
-
-    public Peasant(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100, 0, "Stick", 0.6, 10, 10, 0, position, team);
-        this.endurance = 50;
-    }
-
-    @Override
-    public void getDam(int damage){
-        damage += getRating() + getPower();
-        setHealth(getHealth() - damage);
-    }
-
-    @Override
-    public void atack(Unit opponent, int damage){
-        opponent.getDam(damage);
-        setRating(getRating() + 10);
-    }
-
-    @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", getName(), getPosition());}
->>>>>>> 08d1ffde9ed9158cc4c56f43906082b4fbd8d3ed
 }
