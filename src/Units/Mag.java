85047e7659285f59a216bc57ec3933fe6bb22e03
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-21 16:16:23 +0300

    Update Mag.java

--- src/Units/Mag.java
+++ src/Units/Mag.java
@@ -1,5 +1,4 @@
 package Units;
-<<<<<<< HEAD
 
 import Edits.Coord;
 
@@ -28,39 +27,4 @@ public class Mag extends Unit{
     @Override
     public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
 
-=======
-import java.util.ArrayList;
-
-import Coord;
-
-public class Mag extends Unit{
-
-    private int mana;
-
-    protected Mag(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 110, 20, "Staff", 0.3, 10, 60, 0, position, team);
-        this.mana = 30;
-    }
-
-    public void manaUp() {
-        if(getHealth() < 10){
-            mana += 50;
-        }
-    }
-
-    @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", getName(), getPosition());}
-
-    @Override
-    public void getDam(int damage){
-        damage += getRating() + getPower();
-        setHealth(getHealth() - damage);
-    }
-
-    @Override
-    public void atack(Unit opponent, int damage){
-        opponent.getDam(damage);
-        setRating(getRating() + 10);
-    }
->>>>>>> 08d1ffde9ed9158cc4c56f43906082b4fbd8d3ed
 }
