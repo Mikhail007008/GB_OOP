92df6668602e0e45fe4bf251e647d08e1e3989d6
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-21 16:17:29 +0300

    Update Sniper.java

--- src/Units/Sniper.java
+++ src/Units/Sniper.java
@@ -1,5 +1,4 @@
 package Units;
-<<<<<<< HEAD
 
 import Edits.Coord;
 
@@ -11,23 +10,10 @@ public class Sniper extends Unit {
     public Sniper(String name, Coord position, ArrayList<Unit> team) {
         super(name, 100,100, 30, "Gun", 3, 20, 100, 0, position, team);
         countPatron = 10;
-=======
-import java.util.ArrayList;
-
-import Coord;
-
-public class Sniper extends Unit {
-    private int hiding;
-
-    public Sniper(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100, 30, "Gun", 0.7, 20, 100, 0, position, team);
-        this.hiding = 50;
->>>>>>> 08d1ffde9ed9158cc4c56f43906082b4fbd8d3ed
     }
 
     public void headShoot(){
         boolean headShoot = false;
-<<<<<<< HEAD
         if(!headShoot) rating += 10;
     }
 
@@ -66,31 +52,4 @@ public class Sniper extends Unit {
 
     @Override
     public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
-=======
-        if(!headShoot){
-            setRating(getRating() + 10);
-        }
-    }
-
-    public void target(){
-        Unit enemy = findEnemy();
-        if (enemy != null) System.out.println("Ближайший враг: " + enemy.getName());
-        else System.out.println("Отсутствуют враги в зоне видимости");
-    }
-
-    @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", getName(), getPosition());}
-
-    @Override
-    public void getDam(int damage){
-        damage += getRating() + getPower();
-        setHealth(getHealth() - damage);
-    }
-
-    @Override
-    public void atack(Unit opponent, int damage){
-        opponent.getDam(damage);
-        setRating(getRating() + 10);
-    }
->>>>>>> 08d1ffde9ed9158cc4c56f43906082b4fbd8d3ed
 }
