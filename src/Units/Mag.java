df31dd131be02f9f82a1ee8a8ed8b17bcbcbd6b6
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:33:04 +0300

    Update Mag.java

--- src/Units/Mag.java
+++ src/Units/Mag.java
@@ -4,27 +4,28 @@ import Edits.Coord;
 
 import java.util.ArrayList;
 
-public class Mag extends Unit{
+public class Mag extends Unit {
 
     int mana;
 
-    public Mag(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100,100, 20, "Staff", 1, 10, 60, 0, position, team);
+    public Mag(String name, Coord position) {
+        super(name, 100, 100, 20, "Staff", 1, 10, 60, 0, position);
         mana = 30;
     }
 
     public void manaUp() {
-        if(health < 10){
+        if (health < 10) {
             mana += 50;
         }
     }
-    
+
     @Override
-    public void step() {
-        
+    public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
+
     }
 
     @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
-
+    public String toString() {
+        return String.format("Unit: %s, Name: %s, Coord: %s", getClass().getSimpleName(), name, position);
+    }
 }
