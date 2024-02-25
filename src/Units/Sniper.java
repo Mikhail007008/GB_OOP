4cdad5515b03ef3d13a0c65c3416631c39c1bcee
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:33:57 +0300

    Update Sniper.java

--- src/Units/Sniper.java
+++ src/Units/Sniper.java
@@ -7,49 +7,55 @@ import java.util.ArrayList;
 public class Sniper extends Unit {
     int countPatron;
 
-    public Sniper(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100,100, 30, "Gun", 3, 20, 100, 0, position, team);
+    public Sniper(String name, Coord position) {
+        super(name, 100, 100, 30, "Gun", 3, 20, 100, 0, position);
         countPatron = 10;
     }
 
-    public void headShoot(){
+    public void headShoot() {
         boolean headShoot = false;
-        if(!headShoot) rating += 10;
+        if (!headShoot) rating += 10;
     }
 
     @Override
-    public Unit findEnemy() {
+    public Unit findEnemy(ArrayList<Unit> enemies) {
+        if (enemies.isEmpty()) return null;
+
         Unit closestEnemy = null;
         double minDist = Double.MAX_VALUE;
 
-        for (Unit enemy : team) {
+        for (Unit enemy : enemies) {
             double dist = position.editCoord(enemy.position);
             if (dist < minDist && enemy.health > 0) {
                 minDist = dist;
                 closestEnemy = enemy;
+
                 if (minDist < 3) {
                     power += 20;
                     headShoot();
-                }else if (enemy.bron > 40 || minDist > 3) {
+                } else if (enemy.bron > 40 || minDist > 3) {
                     power -= 20;
                 }
-            } 
+            }
         }
         return closestEnemy;
     }
 
     @Override
-    public void step() {
-        if (health > 0 || countPatron > 0) {
-            Unit enemy = findEnemy();
-            if (enemy != null) {
-                System.out.println("Ближайший враг: " + enemy.name);
-                enemy.getHit(power);
-                countPatron--;
-            }else System.out.println("Отсутствуют враги в зоне видимости");
-        }
+    public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
+        if (health < 0 || countPatron < 0) return;
+
+        Unit target = findEnemy(enemy);
+
+        if (target != null) {
+            System.out.println("Ближайший враг: " + target.name);
+            target.getHit(power);
+            countPatron--;
+        } else System.out.println("Отсутствуют враги в зоне видимости");
     }
 
     @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
+    public String toString() {
+        return String.format("Unit: %s, Name: %s, Coord: %s", getClass().getSimpleName(), name, position);
+    }
 }
