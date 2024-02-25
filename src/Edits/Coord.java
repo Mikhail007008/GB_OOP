d7adab82472b5b0d49e3a23f20b1540c882fe58d
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:31:36 +0300

    Update Coord.java

--- src/Edits/Coord.java
+++ src/Edits/Coord.java
@@ -8,8 +8,32 @@ public class Coord {
         this.y = y;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public double editCoord(Coord enemyCoord) {
         return Math.sqrt(Math.pow(x - enemyCoord.x, 2) + Math.pow(y - enemyCoord.y, 2));
+    }
+
+    public Coord findSpace(Coord enemyCoord) {
+        return new Coord(x - enemyCoord.x, y - enemyCoord.y);
+    }
+
+    public boolean equals(Coord enemy) {
+        return x == enemy.x && y == enemy.y;
     }
 
     @Override
