303b2345d18380e1bd200c33c8733843803db9df
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:33:41 +0300

    Update Peasant.java

--- src/Units/Peasant.java
+++ src/Units/Peasant.java
@@ -7,16 +7,41 @@ import java.util.ArrayList;
 public class Peasant extends Unit {
     int endurance;
 
-    public Peasant(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100,100, 0, "Stick", 0, 10, 10, 0, position, team);
+    public Peasant(String name, Coord position) {
+        super(name, 100, 100, 0, "Stick", 0, 10, 10, 0, position);
         endurance = 50;
     }
 
     @Override
-    public void step() {
-        
+    public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
+        if (health < 0) return;
+
+        Unit target = findEnemy(enemy);
+
+        if (target == null) return;
+
+        if (position.editCoord(target.position) < 2) {
+            System.out.println("Ближайший враг: " + target.name);
+            target.getHit(power);
+            return;
+        }
+
+        Coord space = position.findSpace(target.position);
+        Coord newCoord = new Coord(this.position.getX(), this.position.getY());
+
+        if (Math.abs(space.getX()) > Math.abs(space.getY())) {
+            newCoord.setX(newCoord.getX() + (space.getX() < 0 ? 1 : -1));
+        } else newCoord.setY(newCoord.getY() + (space.getY() < 0 ? 1 : -1));
+
+        for (Unit unit : friend) {
+            if (unit.position.equals(newCoord)) return;
+        }
+
+        this.position = newCoord;
     }
 
     @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
+    public String toString() {
+        return String.format("Unit: %s, Name: %s, Coord: %s", getClass().getSimpleName(), name, position);
+    }
 }
