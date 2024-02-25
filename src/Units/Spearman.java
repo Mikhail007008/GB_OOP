af6e1ef1f4446cd52b93e530987a57855154de20
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:34:29 +0300

    Update Spearman.java

--- src/Units/Spearman.java
+++ src/Units/Spearman.java
@@ -4,19 +4,21 @@ import Edits.Coord;
 
 import java.util.ArrayList;
 
-public class Spearman extends Unit{
+public class Spearman extends Unit {
     int doubleBron;
 
-    public Spearman(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100,100, 80, "spear", 2, 60, 20, 0, position, team);
+    public Spearman(String name, Coord position) {
+        super(name, 100, 100, 80, "spear", 2, 60, 20, 0, position);
         doubleBron = 20;
-    }
-    
-    @Override
-    public void step() {
-        
     }
 
     @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
+    public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Unit: %s, Name: %s, Coord: %s", getClass().getSimpleName(), name, position);
+    }
 }
