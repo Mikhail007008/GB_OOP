1abf2f1eefbb8b3b16c766e9cd6ba60503769429
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:33:22 +0300

    Update Monk.java

--- src/Units/Monk.java
+++ src/Units/Monk.java
@@ -4,27 +4,29 @@ import Edits.Coord;
 
 import java.util.ArrayList;
 
-public class Monk extends Unit{
+public class Monk extends Unit {
     int ironPunch;
 
-    public Monk(String name, Coord position, ArrayList<Unit> team) {
-        super(name, 100,100, 70, "Axe", 2, 70, 10, 0, position, team);
+    public Monk(String name, Coord position) {
+        super(name, 100, 100, 70, "Axe", 2, 70, 10, 0, position);
         ironPunch = 2;
     }
 
     @Override
-    public void getHit(float damage) {
-        health -= (int)damage * ironPunch;
+    public void getHit(int damage) {
+        health -= damage * ironPunch;
         rating += 10;
         if (health < 0) health = 0;
         if (health > maxHealth) health = maxHealth;
     }
 
     @Override
-    public void step() {
-        
+    public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
+
     }
- 
+
     @Override
-    public String toString() {return String.format("Name: %s, Coord: %s", name, position);}
+    public String toString() {
+        return String.format("Unit: %s, Name: %s, Coord: %s", getClass().getSimpleName(), name, position);
+    }
 }
