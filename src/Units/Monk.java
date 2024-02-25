492a314c2de9213d77dffa8e5a43f302ba4da949
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2024-02-26 00:39:07 +0300

    Update Monk.java

--- src/Units/Monk.java
+++ src/Units/Monk.java
@@ -22,7 +22,30 @@ public class Monk extends Unit {
 
     @Override
     public void step(ArrayList<Unit> enemy, ArrayList<Unit> friend) {
+        if (health < 0) return;
 
+        Unit target = findEnemy(enemy);
+
+        if (target == null) return;
+
+        if (position.editCoord(target.position) < 2) {
+            System.out.println("Ближайший враг: " + target.name);
+            target.getHit(power);
+            return;
+        }
+
+        Coord space = position.findSpace(target.position);
+        Coord newCoord = new Coord(this.position.getX(), this.position.getY());
+
+        if (Math.abs(space.getX()) > Math.abs(space.getY())) {
+            newCoord.setX(newCoord.getX() + (space.getX() < 0 ? 1 : -1));
+        } else newCoord.setY(newCoord.getY() + (space.getY() < 0 ? 1 : -1));
+
+        for (Unit unit : friend) {
+            if (unit.position.equals(newCoord)) return;
+        }
+
+        this.position = newCoord;
     }
 
     @Override
